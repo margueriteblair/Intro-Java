0e24d5a4e689bff594c42e7912b1d1676ea23513
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-27 19:29:42 -0400

    primitive types vs object types

---
+++ objects.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MyProgram {
+    public static void main(String[] args) {
+        String string = new String("This is a string!");
+
+        String easier = "Wow, that was easy! " + string;
+        System.out.println(easier);
+
+        int x = 5; //this is a primitive
+        Integer y = 5; //this one is an object instance
+    }
+}
+
+//a data type is a type of data :0 lmao how the computer interprets the data
+//strings vs numbers are different
+//statically typed = all data types upfront
+//variables do not have types for dynamically typed 
+
+//there are primitve types
+//there are objects
+//objects are an instance of a class, class is a data type
+//boolean byte char int long float double are primitive types in java
+
+//we create a primitive type in java
+
