9070f0f79c07489d4f9dfe5e1e3a0eb8ec5cdf36
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-28 16:34:02 -0400

    numeric types in java specifically doubles and ints

---
+++ numericTypes.java
@@ -0,0 +1,10 @@
+public class MySweetProgram {
+    public static void main(String[] args) {
+        int x = 5; //primitive because it's not an object
+        Integer y = 5; //classobject
+        //the integer class has a lot of methods attached to it
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE); //double.infinity
+
+    }
+}
