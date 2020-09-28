0f76b508100f5bbdabff3be20b3fd3687f05a22b
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-27 20:06:12 -0400

    practiced using all other primitive types of java vals

---
+++ primitives2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MySweetProgram() {
+    public static void main(String[] args) {
+        int x = 1;
+        double y = 20.5; //you can have fractional values inside a double
+        boolean isPizzaYummy = true;
+        byte b = 127; //just a small datatype that can store small datatype
+        char c = 'Z'; //char data allows us to store unicode
+        short s = 3456; //short is used for numbers
+        int i = 4343434; //32 bit number, no commas or spaces
+        long l = 23456789876543L;
+        float f = 20.5f; //32 bit, smaller container than a double
+        double hf = 20.5; //64 bit and you don't need to put an f after
+
+        //you can mix number data types for execute expressions
+        //type casting forces
+    }
+    
+}
