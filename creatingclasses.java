bfd0ed8b6d1db57bc49afad0df03d7cc74f76964
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-28 22:31:18 -0400

    created a user class and then used in another file

---
+++ creatingclasses.java
@@ -0,0 +1,11 @@
+public class creatingclasses {
+    public static void main(String[] args) {
+        //classes set structure
+        User user = new User();
+        user.firstName = "Margie";
+        user.lastName = "Blair";
+        System.out.println(user.getFullName());
+
+    }
+}
+
