f4cdcf632e0a1b1535faea6aa5b74465a8b58eea
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-30 19:05:03 -0400

    ternary operator

---
+++ ternaryoperator.java
@@ -0,0 +1,10 @@
+public class ternaryoperator {
+
+    public static void main(String[] args) {
+        String name = "Margie";
+
+        boolean welcome = name.equals("Margie") ? true : false;
+        System.out.println(welcome);
+    }
+    
+}
