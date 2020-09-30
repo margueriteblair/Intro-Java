33875da7041afe1a7ae1fa93d9d9ed062f3a7932
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-30 19:49:58 -0400

    one line if statement

---
+++ singlelineif.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class singlelineif {
+    
+    public static void main(String[] args) {
+        String name = "Margie";
+        boolean welcome = false;
+
+        if (name.equals("Margie")) welcome= true;
+        
+        System.out.println(welcome);
+    }
+}
