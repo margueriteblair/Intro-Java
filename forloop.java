ee5bf55458682bf05a184feedb937da012b71016
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-01 15:31:01 -0400

    for loop in java

---
+++ forloop.java
@@ -0,0 +1,8 @@
+public class forloop {
+    public static void main(String[] args) {
+        //syntactically different from the while loop
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
