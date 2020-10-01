5eb24d315f7152bcf77119b9c9ee2b53d870edf6
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-01 15:33:51 -0400

    nested if statements

--- forloop.java
+++ forloop.java
@@ -1,8 +1,15 @@
 public class forloop {
     public static void main(String[] args) {
+        boolean additionalLogging = false;
         //syntactically different from the while loop
         for(int i = 0; i < 10; i++) {
             System.out.println(i);
+            if (i == 3) {
+                if (additionalLogging == true) {
+                    System.out.println("We found 3");
+
+                }
+            }
         }
     }
 }
