5f7f836fb37c38740c25d819bbf7394695e768d0
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-03 17:02:28 -0400

    creating an instance of an array

---
+++ javaarrays.java
@@ -0,0 +1,12 @@
+public class javaarrays {
+    public static void main(String[] args) {
+        int[] grades = new int[10];
+        System.out.println(grades.length);
+        System.out.println(grades[0]);
+    }
+}
+
+//the syntax is usually dataType[] arrayVarName
+//but it can also be: dataType arrayVarName[]
+//the first way is preferred.
+//arrays in java have to be of the same type of each element.
