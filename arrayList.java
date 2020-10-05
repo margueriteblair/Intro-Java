162311138f6f7119ed6fe3788fa5ad5e90f6e48d
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-05 11:49:55 -0400

    working with array lists

--- arrayList.java
+++ arrayList.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 public class arrayList {
     public static void main(String[] args) {
         List<Integer> grades = new LinkedList<Integer>();
-
+        grades.add(1);
+        grades.add(2);
+        System.out.println(grades);
+        //interesting because you don't need to Arrays.toString();
     }
 }
