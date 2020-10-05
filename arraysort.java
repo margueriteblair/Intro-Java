4a3a49a8c44f024abccfef55e7da0727a61400f4
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-05 01:05:59 -0400

    comparison method for two arrays

--- arraysort.java
+++ arraysort.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 public class arraysort {
     public static void main(String[] args) {
         int[] grades = {1, 2, 3, 72, 5};
-        Arrays.sort(grades);
+        int[] grades2 = {1, 2, 3, 72, 5};
+        // Arrays.sort(grades);
         // Arrays.parallelSort();
-        System.out.println(Arrays.toString(grades));
+        // System.out.println(Arrays.toString(grades));
+
+        System.out.println((Arrays.equals(grades, grades2)));
     }
 }
