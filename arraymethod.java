0a12e461f845cd95ebf6a00db08d49ef17c3a1c1
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-04 23:34:14 -0400

    printing out an array and a multidimensional array using methods

--- arraymethod.java
+++ arraymethod.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class arraymethod {
     public static void main(String[] args) {
         int[] grades = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println(grades[6]);
+        System.out.println(Arrays.toString(grades));
+
+        int[][] grades2 = {{1, 2}, {3, 4, 5}, {6, 7, 8, 9}};
+        System.out.println(Arrays.deepToString(grades2));
     }
 }
