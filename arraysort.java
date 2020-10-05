a83ed9201a3f240dd42451f0ac2c510c14d7208e
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-05 00:35:34 -0400

    used parallel sort and serial sort to sort an array

---
+++ arraysort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class arraysort {
+    public static void main(String[] args) {
+        int[] grades = {1, 2, 3, 72, 5};
+        Arrays.sort(grades);
+        // Arrays.parallelSort();
+        System.out.println(Arrays.toString(grades));
+    }
+}
