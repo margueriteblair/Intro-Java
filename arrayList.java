eef6b5c1e26d3c6820368ac8a1410df981d3e5e4
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-05 11:39:03 -0400

    made an interface and an implementation for an arrayList

---
+++ arrayList.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        List<Integer> grades = new LinkedList<Integer>();
+
+    }
+}
