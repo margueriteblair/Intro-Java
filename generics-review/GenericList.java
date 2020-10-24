2e290fd8deacf432c5e0f91ae0bc8d6550150a82
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-24 18:20:47 -0400

    practicing putting restraints on generic lists

--- generics-review/GenericList.java
+++ generics-review/GenericList.java
@@ -1,6 +1,10 @@
 
-public class GenericList<T> {
+public class GenericList<T extends Comparable & Cloneable> {
+    //T extends Number means that T will only be able to support
+    //children of the Number class, in wrapper form of course
+    //this assumes we'll make some methods that are only applicable for numbers
     // T represents the type of objects
+    //comparable interface
     private T[] items = (T[]) new Object[10]; 
     private int count;
 
