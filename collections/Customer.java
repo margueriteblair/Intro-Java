3751d5d28c59415d3cf86208eb184b1b776eea09
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-24 22:58:27 -0400

    using the comparable interface to compare two names

---
+++ collections/Customer.java
@@ -0,0 +1,20 @@
+package collections;
+
+public class Customer implements Comparable<Customer> {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Customer other) {
+        return name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+        //it's very common to override the toString method
+    }
+}
--- collections/Main.java
+++ collections/Main.java
@@ -1,7 +1,16 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        CollectionsDemo.show();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("B"));
+        customers.add(new Customer("C"));
+        customers.add(new Customer("BA"));
+        Collections.sort(customers);
+
     }
 }
