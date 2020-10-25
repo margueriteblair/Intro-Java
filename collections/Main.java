1a31a875cc1bb0a12954dc99208740afccdeb232
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-24 21:43:40 -0400

    rewrote generic list and main class

---
+++ collections/Main.java
@@ -0,0 +1,9 @@
+package collections;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new GenericList<String>();
+        list.add("a");
+        list.add("b");
+    }
+}
