8ac1e5328e6aab573b2458d75296713e700fdb97
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-24 22:01:15 -0400

    new list iterator and methods

--- collections/GenericList.java
+++ collections/GenericList.java
@@ -1,6 +1,7 @@
 package collections;
 
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class GenericList<T> implements Iterable<T> {
 
@@ -20,6 +21,28 @@ public class GenericList<T> implements Iterable<T> {
     public Iterator<T> iterator() {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private class ListIterator implements Iterator<T> {
+        private GenericList<T> list;
+        private int index;
+
+        public ListIterator(GenericList<T> list) {
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            // TODO Auto-generated method stub
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            // TODO Auto-generated method stub
+            return list.items[index++];
+        }
+        
     }
     
 }
