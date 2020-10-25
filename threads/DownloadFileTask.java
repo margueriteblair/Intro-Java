121d41e49f2cfda05d54eeca0651bdae7daaa789
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-25 17:28:38 -0400

    pausing an application with thread sleep

--- threads/DownloadFileTask.java
+++ threads/DownloadFileTask.java
@@ -5,5 +5,11 @@ public class DownloadFileTask implements Runnable {
     public void run() {
         System.out.println("Downloading a file...");
         System.out.println(Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
