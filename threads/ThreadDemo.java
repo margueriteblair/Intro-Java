c1cd70b3f9ef465dcefa38c5aab4a68139458861
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-25 17:33:13 -0400

    joining a thread to have it wait for another thread

--- threads/ThreadDemo.java
+++ threads/ThreadDemo.java
@@ -5,5 +5,14 @@ public class ThreadDemo {
         System.out.println(Thread.currentThread().getName());
         Thread thread = new Thread(new DownloadFileTask());
         thread.start();
+
+        try {
+
+            thread.join();
+            System.out.println("File is ready to be scanned.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         //this will block the current thread until the downloaded thread has finished
     }
 }
