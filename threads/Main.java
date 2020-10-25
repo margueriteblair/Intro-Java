4f7bdb473c1de6e53b4228b6a640e6c2ebe2a535
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-25 17:06:28 -0400

    There are currently 1 thread running and 4 available processors

---
+++ threads/Main.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+    
+}
