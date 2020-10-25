6c59cac5453d41bcc826485581377194a93f7c93
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-25 15:38:33 -0400

    integer object to reduce and sum a stream

--- streams/StreamsDemo.java
+++ streams/StreamsDemo.java
@@ -2,6 +2,7 @@ package streams;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class StreamsDemo {
@@ -14,10 +15,11 @@ public class StreamsDemo {
 
         );
 
-        var accum = movies.stream()
+        Optional<Integer> sum = movies.stream()
         .map(m -> m.getLikes())
         .reduce((a, b) -> a + b);
 
+        System.out.println(sum.orElse(0));
     }
 
 
