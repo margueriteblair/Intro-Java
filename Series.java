2f8c430759b11183705cba90ef5b7f9721985454
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-02 14:53:32 -0500

    private method within an interface

--- Series.java
+++ Series.java
@@ -5,4 +5,8 @@ public interface Series {
     default void printStuff() {
         System.out.println("Live from NY, it's Saturday night!");
     }
+
+    private void printMoreStuff() {
+        System.out.println("Print more stuff");
+    }
 }
